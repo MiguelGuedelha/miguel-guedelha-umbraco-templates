01fc102fff755fa8eadd3bbd4f886cdaa3daf075
MiguelGuedelha <miguel.guedelha.business@gmail.com>
2025-12-09 19:23:08 +0000

    clean bom off from files

--- project-templates/content/umbraco-headless-bff/UmbracoHeadlessBFF.SiteApi/src/UmbracoHeadlessBFF.SiteApi.Modules.Pages/Mappers/Components/BlogListingMapper.cs
+++ project-templates/content/umbraco-headless-bff/UmbracoHeadlessBFF.SiteApi/src/UmbracoHeadlessBFF.SiteApi.Modules.Pages/Mappers/Components/BlogListingMapper.cs
@@ -1,4 +1,4 @@
-﻿using UmbracoHeadlessBFF.SharedModules.Cms.DeliveryApi;
+using UmbracoHeadlessBFF.SharedModules.Cms.DeliveryApi;
 using UmbracoHeadlessBFF.SharedModules.Cms.DeliveryApi.Components;
 using UmbracoHeadlessBFF.SharedModules.Cms.DeliveryApi.Data;
 using UmbracoHeadlessBFF.SiteApi.Modules.Pages.Models.Components;
